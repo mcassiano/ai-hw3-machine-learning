92c2fae25892f3998c35b85f4b3ec111ba6b80b7
Matheus Cassiano Candido <matheus@mcassiano.com>
2014-11-19 03:27:07 -0500

    check on 1.3c and cost function (error is too low?), but Problem 1.1 finished

--- Problem1_1.py
+++ Problem1_1.py
@@ -1,31 +1,59 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import LinearRegression as lr
+from LinearRegression import LinearRegression
 
 def main():
 
-	# 1.1
-	data = np.genfromtxt('girls_train.csv', delimiter=',')
+    # 1.1
+    data = np.genfromtxt('girls_train.csv', delimiter=',')
+    lr = LinearRegression()
 
-	ones = np.ones(len(data))
-	x = np.array([ones, data[:,0]]).transpose()
-	y = data[:,1]
+    ones = np.ones(len(data))
+    x = np.array([ones, data[:,0]]).transpose()
+    y = data[:,1]
 
-	plt.scatter(x[:,1], y)
+    #plt.scatter(x[:,1], y)
 
-	# 1.2
-	
-	m, n = x.shape
-	learning_rate = 0.05
-	number_of_iterations = 1500
-	theta = np.ones(n)
-	theta, history = lr.gradient_descent(x, y, theta, learning_rate, m, number_of_iterations)
+    # 1.2
+    
+    m, n = x.shape
+    learning_rate = 0.05
+    number_of_iterations = 1500
+    theta = np.zeros(n)
+    theta, history, cost = lr.gradient_descent(x, y, theta, learning_rate, number_of_iterations)
 
-	prediction = np.dot(x, theta)
-	
-	plt.plot(x[:,1], prediction)
-	plt.show()
+    X, Y = np.meshgrid(history[:,0], history[:,1])
+    Z = np.ones(shape=X.shape)*cost.T
+
+    prediction = np.dot(x, theta)
+    
+    # 1.3
+    
+    #plt.plot(x[:,1], prediction)
+    #plt.show()
+    
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111, projection='3d')
+    #surf = ax.plot_surface(X, Y, Z)
+
+    # 1.4
+    
+    print "Model: %fx + %f" % (theta[1], theta[0])
+    print "Mean square error for training: %f" % cost[-1]
+    print "Predicted height for a 4.5 years old girl: %f" % (4.5*theta[1] + theta[0])
+
+    test_data = np.genfromtxt('girls_test.csv', delimiter=',')
+    test_x = np.array([np.ones(len(test_data)), test_data[:,0]]).transpose()
+    test_y = test_data[:,1]
+    test_prediction = np.dot(test_x, theta)
+    error = lr.calculate_cost(test_prediction - test_y, len(data))
+    
+    print "Mean square error for testing: %f" % (error)
+
+
+    #plt.show()
 
 
 if __name__ == "__main__":
-	main()
+    main()
