023c66d18d49f55fc6c7121bdaa0c5843561af57
Matheus Cassiano Candido <matheus@mcassiano.com>
2014-11-20 18:22:54 -0500

    Fixed #1

--- Problem2.py
+++ Problem2.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn import cross_validation
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.grid_search import GridSearchCV
 
 def plot_data(model, fig, x, y):
 
@@ -51,63 +52,57 @@ def main():
     
     k_folds = [5, 10]
     k_neighbors = [1,2,3,4,5,6,7,8]
-    degrees = [5,6]
-    gammas = [0.01, 0.1, 1, 10]
-    cs = [0.1, 1]
+    degrees = [4]
+    gammas = [1]#[0.01, 0.1, 1, 10]
+    cs = [1]#[0.01, 0.1, 1, 10]
 
-    lin_scores = []
-    poly_scores = []
-    rbf_scores = []
-    knn_scores = []        
+    params_grid_svm = {
+        'rbf': [{'kernel': ['rbf'], 'gamma': gammas, 'C': cs}],
+        'linear': [{'kernel': ['linear'], 'C': cs}],
+        'poly': [{'kernel': ['poly'], 'C': cs, 'degree': degrees}],
+    }
+
+    params_grid_knn = [
+        {'n_neighbors': k_neighbors}
+    ]
+
+    scores = {}
+    for classifier in ['rbf', 'linear', 'poly', 'knn']:
+        scores[classifier] = []
 
     for k in k_folds:
+        for kernel, params in params_grid_svm.items():
+            clf = GridSearchCV(svm.SVC(), params, cv=k)
+            clf.fit(X_train, y_train)
+            scores[kernel].append([clf.best_score_, k, clf.best_params_])
 
-        for kn in k_neighbors:
-            knn = KNeighborsClassifier(n_neighbors=kn)
-            scores = cross_validation.cross_val_score(
-                        knn, X_train, y_train, cv=k)
-            knn_scores.append([scores.mean(), k, kn])
+    for k in k_folds:
+        clf = GridSearchCV(KNeighborsClassifier(), params_grid_knn, cv=k)
+        clf.fit(X_train, y_train)
+        scores['knn'].append([clf.best_score_, k, clf.best_params_])
 
-        for c in cs:
-            linear_svc = svm.SVC(kernel='linear', C=c)
-            scores = cross_validation.cross_val_score(
-                linear_svc, X_train, y_train, cv=k)
-            lin_scores.append([scores.mean(), k, c])
+    best_rbf = max(scores['rbf'], key = lambda s: s[0])
+    best_linear = max(scores['linear'], key = lambda s: s[0])
+    best_poly = max(scores['poly'], key = lambda s: s[0])
+    best_knn = max(scores['knn'], key = lambda s: s[0])
 
-            for gamma in gammas:
-                rbf_svc = svm.SVC(kernel='rbf', gamma=gamma, C=c)
-                scores = cross_validation.cross_val_score(
-                    rbf_svc, X_train, y_train, cv=k)
-                rbf_scores.append([scores.mean(), k, c, gamma])
 
-            for degree in degrees:
-                poly_svc = svm.SVC(kernel='poly', degree=degree, C=c)
-                scores = cross_validation.cross_val_score(
-                    poly_svc, X_train, y_train, cv=k)
-                poly_scores.append([scores.mean(), k, c, degree])
-
-    
-    best_linear = max(lin_scores, key = lambda s: s[0])
-    best_poly = max(poly_scores, key = lambda s: s[0])
-    best_rbf = max(rbf_scores, key = lambda s: s[0])
-    best_knn = max(knn_scores, key = lambda s: s[0])
-
-    lin = svm.SVC(kernel='linear', C=best_linear[2]).fit(X_test, y_test)
-    poly = svm.SVC(kernel='poly', degree=best_poly[3], C=best_poly[2]).fit(X_test, y_test)
-    rbf = svm.SVC(kernel='rbf', gamma=best_rbf[3], C=best_rbf[2]).fit(X_test, y_test)
-    knn = KNeighborsClassifier(n_neighbors=best_knn[2]).fit(X_train, y_train)
+    lin = svm.SVC(**best_linear[2]).fit(X_test, y_test)
+    poly = svm.SVC(**best_poly[2]).fit(X_test, y_test)
+    rbf = svm.SVC(**best_rbf[2]).fit(X_test, y_test)
+    knn = KNeighborsClassifier(**best_knn[2]).fit(X_train, y_train)
 
     plot_data(lin, graph_linear, X_test, y_test)
-    graph_linear.set_title("Linear. C = %0.1f" % best_linear[2], fontsize=12)
+    graph_linear.set_title("Linear. C = %0.1f" % best_linear[2]['C'], fontsize=10)
 
     plot_data(poly, graph_poly, X_test, y_test)
-    graph_poly.set_title("Poly. C = %0.1f, d = %d" % (best_poly[2], best_poly[3]), fontsize=12)
+    graph_poly.set_title("Poly. C = %0.1f, d = %d" % (best_poly[2]['C'], best_poly[2]['degree']), fontsize=10)
 
     plot_data(rbf, graph_rbf, X_test, y_test)
-    graph_rbf.set_title("RBF C = %0.1f, g = %0.1f" % (best_rbf[2], best_rbf[3]), fontsize=12)
+    graph_rbf.set_title("RBF C = %0.1f, g = %0.1f" % (best_rbf[2]['C'], best_rbf[2]['gamma']), fontsize=10)
 
     plot_data(knn, graph_knn, X_test, y_test)
-    graph_knn.set_title("K-Nearest neighbors. K = %d" % best_knn[2], fontsize=12)
+    graph_knn.set_title("K-Nearest neighbors. K = %d" % best_knn[2]['n_neighbors'], fontsize=10)
 
 
     plt.show()
