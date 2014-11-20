789de72766bc923fd677252c6ee5762149b129a6
Matheus Cassiano Candido <matheus@mcassiano.com>
2014-11-19 19:00:34 -0500

    Finished Questions 1,2 and 3 from Problem 1

--- Problem1_2.py
+++ Problem1_2.py
@@ -26,20 +26,37 @@ def main():
 
     x[:,1] = (x[:,1] - age_mean)/age_std
     x[:,2] = (x[:,2] - weight_mean)/weight_std
-
-    # 2.2
     
     m, n = x.shape
-    learning_rate = 0.05
-    number_of_iterations = 1500
-    theta = np.zeros(n)
-    theta = lr.gradient_descent(x, y, theta, learning_rate, number_of_iterations)
 
     # 2.3
     
-    alphas = [0.005, 0.001, 0.05, 0.1, 0.5, 1]
+    alphas = [0.005, 0.001, 0.05, 0.1, 0.5, 1.0]
+    iterations_n = 50
+    iterations = np.arange(iterations_n)
+    risk = np.zeros(shape = (iterations_n, len(alphas))).T
 
+    for alpha_i in range(0, len(alphas)):
+        theta_sim = np.zeros(n)
+        for iteration_n in iterations:
+            theta_sim = lr.gradient_descent(x, y, theta_sim, alphas[alpha_i], iteration_n)
+            prediction = np.dot(x, theta_sim)
+            loss = prediction - y
+            risk[alpha_i][iteration_n] = lr.calculate_cost(loss, m)
 
+    for alpha_i in range(0, len(alphas)):
+        plt.plot(iterations, risk[alpha_i], label='Alpha: %f' % alphas[alpha_i])
+
+    theta = lr.gradient_descent(x,y,np.zeros(n), 1.0, 50)
+    prediction = np.dot(x, theta)
+
+    point_to_guess = [1.0, (5.0-age_mean)/age_std, (20.0-weight_mean)/weight_std]
+    guess = np.sum(np.dot(theta, point_to_guess))
+
+    print "The 5 year girl weighting 20 is approximately %f m tall." % (guess)
+
+    plt.legend()
+    plt.show()
 
 
 
